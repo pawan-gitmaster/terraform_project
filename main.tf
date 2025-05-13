c9b6734ea363a8d57e26c49e7cc1aafe1b023574
pawan-gitmaster <102990234+pawan-gitmaster@users.noreply.github.com>
2025-05-13 17:59:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "module1" {
     source = "https://github.com/pawan-gitmaster/terraform_project/tree/main/module1"
-    ami = var.ami_image_var
-    instance_type = var.instance_type_var
+    ami_image_var = var.ami_image_var
+    ami_image_var = var.ami_image_var
   
 }
